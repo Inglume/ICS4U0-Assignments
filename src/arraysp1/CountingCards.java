fad2c033a89d49753f9cc591aeb33b011639ecc7
Inglume <inglume@gmail.com>
2019-04-13 14:04:14 -0400

    Finished CountingCards

--- src/arraysp1/CountingCards.java
+++ src/arraysp1/CountingCards.java
@@ -1,8 +1,8 @@
 package arraysp1;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
@@ -100,35 +100,37 @@ public class CountingCards {
   /**
    * Sets the static variable cards according to the file
    */
-  private static void resetCards() {
+  private static void setCards() {
     Card[][] cards = new Card[4][13];
     try {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      for (int i = 0; i < 4; i++) {
+      BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+      br.readLine();
+      for (int i = 0; i < 4; i++) { // loop once per hand
         String input = br.readLine();
-        if (input.length() == 26) {
+        if (input.length() == 26) { // if hand length is valid
           for (int j = 0; j < 26; j += 2) {
             char rank = 0;
             char suit = 0;
             char temp = 0;
             if (((temp = input.charAt(j)) >= '2' && temp <= '9') || temp == 'T' || temp == 'J'
-                || temp == 'Q' || temp == 'K' || temp == 'A') {
+                || temp == 'Q' || temp == 'K' || temp == 'A') { // if rank is valid
               rank = temp;
-            } else {
+            } else { // if rank is invalid
               System.out.println("Invalid hand, use appropriate characters.");
               bad[i] = true;
               break;
             }
+            // if suit is valid
             if ((temp = input.charAt(j + 1)) == 'S' || temp == 'H' || temp == 'C' || temp == 'D') {
               suit = temp;
-            } else {
+            } else { // if suit is invalid
               System.out.println("Invalid hand, use appropriate characters.");
               bad[i] = true;
               break;
             }
             cards[i][j / 2] = new Card(rank, suit);
           }
-        } else {
+        } else { // if hand length is invalid
           System.out.println("Invalid hand, hand must have 13 cards.");
           bad[i] = true;
         }
@@ -136,9 +138,9 @@ public class CountingCards {
     } catch (IOException e) {
     }
     for (int i = 0; i < 4; i++) {
-      if (!bad[i]) {
+      if (!bad[i]) { // if hand is valid
         for (int j = 0; j < 13; j++) {
-          CountingCards.cards[i][j] = cards[i][j];
+          CountingCards.cards[i][j] = cards[i][j]; // add local cards to static cards
         }
       }
     }
@@ -154,25 +156,25 @@ public class CountingCards {
    */
   private static void merge(ArrayList<Card> list, int l, int m, int r) {
     ArrayList<Card> temp = new ArrayList<Card>();
-    int i = l, j = m + 1;
+    int i = l, j = m + 1; // i is loop index for left side, j for right side
     while (i <= m && j <= r) {
-      if (list.get(i).compareTo(list.get(j)) == 1) {
+      if (list.get(i).compareTo(list.get(j)) == 1) { // if list[i] > list[j]
         temp.add(list.get(i));
         i++;
-      } else {
+      } else { // if list[i] < list[j]
         temp.add(list.get(j));
         j++;
       }
     }
-    while (i <= m) {
+    while (i <= m) { // add rest of left side
       temp.add(list.get(i));
       i++;
     }
-    while (j <= r) {
+    while (j <= r) { // add rest of right side
       temp.add(list.get(j));
       j++;
     }
-    for (int n = l; n <= r; n++) {
+    for (int n = l; n <= r; n++) { // change actual list
       list.set(n, temp.get(n - l));
     }
   }
@@ -185,11 +187,11 @@ public class CountingCards {
    * @param r - right bound of sort
    */
   private static void sort(ArrayList<Card> list, int l, int r) {
-    if (l < r) {
+    if (l < r) { // stop once left and right get too small
       int m = (r + l) / 2;
-      sort(list, l, m);
-      sort(list, m + 1, r);
-      merge(list, l, m, r);
+      sort(list, l, m); // sort left side
+      sort(list, m + 1, r); // sort right side
+      merge(list, l, m, r); // merge left and right side
     }
   }
 
@@ -199,10 +201,11 @@ public class CountingCards {
    * @param list - ArrayList to be sorted
    */
   private static void sort(ArrayList<Card> list) {
-    int l = 0, r = list.size() - 1, m = (r + l) / 2;
-    sort(list, l, m);
-    sort(list, m + 1, r);
-    merge(list, l, m, r);
+    int l = 0, r = list.size() - 1, m = (r + l) / 2; // l is first index, r is last index, m is
+                                                     // middle index
+    sort(list, l, m); // sort left side
+    sort(list, m + 1, r); // sort right side
+    merge(list, l, m, r); // merge left and right side
   }
 
   /**
@@ -213,11 +216,11 @@ public class CountingCards {
    */
   private static int suitNum(ArrayList<Card> list) {
     int sum = 0;
-    if (list.size() == 0) {
+    if (list.size() == 0) { // if suit is void
       sum += 3;
-    } else if (list.size() == 1) {
+    } else if (list.size() == 1) { // if suit is singleton
       sum += 2;
-    } else if (list.size() == 2) {
+    } else if (list.size() == 2) { // if suit is doubleton
       sum++;
     }
     return sum;
@@ -228,7 +231,7 @@ public class CountingCards {
    */
   private static void evaluate() {
     for (int i = 0; i < 4; i++) {
-      if (!bad[i]) {
+      if (!bad[i]) { // if hand is valid
         int points = 0;
         ArrayList<Card> spades = new ArrayList<Card>();
         ArrayList<Card> hearts = new ArrayList<Card>();
@@ -252,18 +255,20 @@ public class CountingCards {
             spades.add(card);
           } else if (temp == 'H') {
             hearts.add(card);
-          } else if (temp == 'C') {
-            clubs.add(card);
           } else if (temp == 'D') {
             diamonds.add(card);
+          } else if (temp == 'C') {
+            clubs.add(card);
           }
         }
 
+        // add points for number of suits
         points += suitNum(spades);
         points += suitNum(hearts);
-        points += suitNum(clubs);
         points += suitNum(diamonds);
+        points += suitNum(clubs);
 
+        // output
         System.out.println("\nHand " + (i + 1));
 
         System.out.print("Spades: ");
@@ -282,19 +287,19 @@ public class CountingCards {
           System.out.print(c + " ");
         }
 
-        System.out.print("\nClubs: ");
-        if (clubs.size() > 1) {
-          sort(clubs);
-        }
-        for (Card c : clubs) {
-          System.out.print(c + " ");
-        }
-
         System.out.print("\nDiamonds: ");
         if (diamonds.size() > 1) {
           sort(diamonds);
         }
         for (Card c : diamonds) {
+          System.out.print(c + " ");
+        }
+
+        System.out.print("\nClubs: ");
+        if (clubs.size() > 1) {
+          sort(clubs);
+        }
+        for (Card c : clubs) {
           System.out.print(c + " ");
         }
 
@@ -310,11 +315,12 @@ public class CountingCards {
    * @throws IOException - thrown by BufferedReader br
    */
   public static void main(String[] args) throws IOException {
-    // BufferedReader br = new BufferedReader(new FileReader("file.txt"));
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // In file.txt, the first line should have the number of sets of hands to follow.
+    // The rest of the lines will have the hands normally as specified on the assignment sheet.
+    BufferedReader br = new BufferedReader(new FileReader("file.txt"));
     int n = Integer.parseInt(br.readLine());
     for (int i = 0; i < n; i++) {
-      resetCards();
+      setCards();
       evaluate();
     }
   }
